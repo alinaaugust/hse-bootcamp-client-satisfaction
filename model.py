591404a31f76e74dbf06f95d0a2693508b26305c
alinaaugust <alinaavgustenok@gmail.com>
2023-08-27 19:31:24 +0300

    tried to fix satisfaction 4

--- model.py
+++ model.py
@@ -18,6 +18,7 @@ DATASET_PATH = "https://raw.githubusercontent.com/evgpat/edu_stepik_from_idea_to
 
 
 def split_data(df: pd.DataFrame):
+    df['satisfaction'] = df['satisfaction'].map({'neutral or dissatisfied' : 0, 'satisfied' : 1})
     data = df.loc[df['satisfaction'].isin([0, 1])]
     X = data.drop(['satisfaction', 'id'], axis=1)
     y = data['satisfaction']
@@ -34,35 +35,35 @@ def open_data():
 def preprocess_data(df: pd.DataFrame, test=True):
     df.dropna(inplace=True)
 
-    categories = ['Inflight wifi service', 'Departure/Arrival time convenient', 'Ease of Online booking', 'Gate location', 'Food and drink', 'Online boarding', 'Seat comfort', 'Inflight entertainment', 'On-board service', 'Leg room service', 'Baggage handling', 'Checkin service', 'Inflight service', 'Cleanliness']
-    for category in categories:
-        df[category] = np.where(df[category] < 1, 1, df[category]) #согласно описанию, все оценки должны быть от 1 до 5, заменим нулевые на минимально возможные
-        df[category] = np.where(df[category] > 5, 5, df[category]) #аналогично поступим с максимальными оценками
-        category_mean = df[category].mean()
-        df[category].fillna(category_mean, inplace=True) #пропуски заполним средним значением для каждой категории
-
-    columns = ['Departure Delay in Minutes', 'Arrival Delay in Minutes']
-    df = df.dropna(axis='index', how='any', subset=['Age'])
-    df['Age'] = np.where(df['Age'] < 10, 10, df['Age'])
-    for col in columns:
-        df[col] = np.where(df[col] > 100, 100, df[col])
-        col_mean = df[col].mean()
-        df[col].fillna(col_mean, inplace=True)
-    df['Flight Distance'] = np.where(df['Flight Distance'] > 7000, 7000, df['Flight Distance'])
-    col_mean = df['Flight Distance'].mean()
-    df['Flight Distance'].fillna(col_mean, inplace=True)
-
-    df = df.dropna(axis='index', how='any', subset=['Gender', 'Customer Type', 'Type of Travel', 'Class'])
-
-    df['Gender'] = df['Gender'].map({'Male' : 0, 'Female' : 1})
-    df['Customer Type'] = df['Customer Type'].map({'disloyal Customer' : 0, 'Loyal Customer' : 1})
-    df['Type of Travel'] = df['Type of Travel'].map({'Business travel' : 0, 'Personal Travel' : 1})
-    df['satisfaction'] = df['satisfaction'].map({'neutral or dissatisfied' : 0, 'satisfied' : 1})
-
     if test:
         X_df, y_df = split_data(df)
     else:
         X_df = df
+
+    categories = ['Inflight wifi service', 'Departure/Arrival time convenient', 'Ease of Online booking', 'Gate location', 'Food and drink', 'Online boarding', 'Seat comfort', 'Inflight entertainment', 'On-board service', 'Leg room service', 'Baggage handling', 'Checkin service', 'Inflight service', 'Cleanliness']
+    for category in categories:
+        X_df[category] = np.where(X_df[category] < 1, 1, X_df[category]) #согласно описанию, все оценки должны быть от 1 до 5, заменим нулевые на минимально возможные
+        X_df[category] = np.where(X_df[category] > 5, 5, X_df[category]) #аналогично поступим с максимальными оценками
+        category_mean = X_df[category].mean()
+        X_df[category].fillna(category_mean, inplace=True) #пропуски заполним средним значением для каждой категории
+
+    columns = ['Departure Delay in Minutes', 'Arrival Delay in Minutes']
+    X_df = X_df.dropna(axis='index', how='any', subset=['Age'])
+    X_df['Age'] = np.where(X_df['Age'] < 10, 10, X_df['Age'])
+    for col in columns:
+        X_df[col] = np.where(X_df[col] > 100, 100, X_df[col])
+        col_mean = X_df[col].mean()
+        X_df[col].fillna(col_mean, inplace=True)
+    X_df['Flight Distance'] = np.where(X_df['Flight Distance'] > 7000, 7000, X_df['Flight Distance'])
+    col_mean = X_df['Flight Distance'].mean()
+    X_df['Flight Distance'].fillna(col_mean, inplace=True)
+
+    X_df = X_df.dropna(axis='index', how='any', subset=['Gender', 'Customer Type', 'Type of Travel', 'Class'])
+
+    X_df['Gender'] = X_df['Gender'].map({'Male' : 0, 'Female' : 1})
+    X_df['Customer Type'] = X_df['Customer Type'].map({'disloyal Customer' : 0, 'Loyal Customer' : 1})
+    X_df['Type of Travel'] = X_df['Type of Travel'].map({'Business travel' : 0, 'Personal Travel' : 1})
+
 
     if test:
         return X_df, y_df
